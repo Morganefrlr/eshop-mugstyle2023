6829d653ee1f3a4c80a5bcffc7806e3e8cce85fa
Morganefrlr <morganeancelin@me.com>
2023-11-02 11:24:42 +0100

    firebase

--- src/app/administrator/add/page.tsx
+++ src/app/administrator/add/page.tsx
@@ -13,70 +13,73 @@ const addProductPageTest = () => {
 
 
     
-    const [file, setFile] = useState<File[]>([])
-    const [media, setMedia] = useState<String[]>([])
+    const [files, setFiles] = useState<FileList | null>(null);
+    const [media, setMedia] = useState<string[]>([])
     const [title, setTitle] = useState('')
-
-    const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>) =>{
-        const target = e.target as HTMLInputElement
-        const item = (target.files as FileList)[0]
-        setFile([...file, item])
+    const[file,setFile] = useState<string>()
+    if(file && !media.includes(file)){
+        media.push(file)
     }
 
 
 
-
     useEffect(()=>{
-        const upload = () =>{
-            if(file){
-              file.forEach((el) => { const name = new Date().getTime + el.name
-               const storageRef = ref(storage, name);
-
-                const uploadTask = uploadBytesResumable(storageRef, el);
-                uploadTask.on('state_changed', 
-                (snapshot) => {
+        const uploadFiles = () =>{
+            if(files){
+                for(let i = 0;i < files.length; i++) {
+                    const name = new Date().getTime + files[i].name
+                    const storageRef = ref(storage, name);
+    
+                    const uploadTask = uploadBytesResumable(storageRef, files[i]);
+    
+                    uploadTask.on('state_changed', 
+                    (snapshot) => {
+                        
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                        }
+                    }, 
+                    (error) => {
+                    console.log(error)
+                    }, 
+                    () => {
                     
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log('Upload is ' + progress + '% done');
-                    switch (snapshot.state) {
-                    case 'paused':
-                        console.log('Upload is paused');
-                        break;
-                    case 'running':
-                        console.log('Upload is running');
-                        break;
-                    }
-                }, 
-                (error) => {
-                console.log(error)
-                }, 
-                () => {
-                
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        const url = downloadURL
-                        setMedia([...media,url])
-                    });
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            const url = downloadURL    
+                            setFile(downloadURL)                   
+                        });
+                    })
+                    
                 }
-                );})
-            } 
             }
+        }
             
-        file && upload()
-    },[file])
+        
+            
+        uploadFiles()
+    },[files])
 
+
+    console.log(media)
    
 
-
-
+    
     return (
         <div className="flex flex-col gap-10 items-center mt-20 max-md:h-auto w-[90vw] mx-auto max-xl:w-[80%]">
             <h1 className="text-3xl mb-20 uppercase">Add Product</h1>
-                <input className="ring-1 ring-red-500 p-2 rounded-sm" type="file" id="files" style={{display : 'none'}} onChange={handleChangeImg}/>
+                <input className="ring-1 ring-red-500 p-2 rounded-sm" type="file" id="files" style={{display : 'none'}} multiple onChange={(e) => setFiles(e.target.files)}/>
                 <label htmlFor="files"><MdAddPhotoAlternate className='text-3xl cursor-pointer mb-10'/></label>
-                    {file && file.length !== 0 &&
+                {media && media.length !== 0 &&
                         <div className="w-full flex justify-center gap-5 my-20 max-sm:flex-wrap">
-                        {file.length && file.map((img, index) =>
-                            <Image src={URL.createObjectURL(img)} key={index} alt='' width={200} height={200} className="w-20 h-20 object-cover"/>
+                        {media.map((img: string, index )=>
+                            <Image src={img} key={index} alt='' width={200} height={200} className="w-20 h-20 object-cover"/>
                             )}
                         </div>
                     }
@@ -105,3 +108,4 @@ const addProductPageTest = () => {
 };
 
 export default addProductPageTest;
+
