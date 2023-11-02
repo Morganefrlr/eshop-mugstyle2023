0c9392e227d31b9e13b84004ac79d4d9d87d8156
Morganefrlr <morganeancelin@me.com>
2023-11-02 12:02:42 +0100

    add product featured

--- src/app/administrator/products/page.tsx
+++ src/app/administrator/products/page.tsx
@@ -13,7 +13,7 @@ const UpdateProductsPage = () => {
     return (
         <div className='w-[90vw] mx-auto flex flex-col items-center mt-20 max-xl:w-[80%]'>
             <h1 className="text-3xl mb-20 uppercase">Update Products</h1>
-            <Link href='/administrator/add' ><BsFillPlusCircleFill className="text-3xl text-zinc-900 mt-10 mb-20 cursor-pointer"/></Link>
+            <Link href='/administrator/products/add' ><BsFillPlusCircleFill className="text-3xl text-zinc-900 mt-10 mb-20 cursor-pointer"/></Link>
             <ProductsList update={true} home={false} />
         </div>
     );
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,13 +1,32 @@
-import { featuredProducts } from "@/types";
+'use client'
+import { ProductType } from "@/types";
 import Card from "./Card";
+import { useQuery } from "react-query";
+
 
 
 const Featured = () => {
+
+   
+
+    const {isLoading, error, data} = useQuery({
+        queryKey:['products'],
+        queryFn: () =>
+        fetch(`http://localhost:3000/api/products/`).then(
+            (res) => res.json()
+        )
+    })
+
+   if(isLoading ) return 'loading ....'
+   
+   const isFeatured = data.filter((product :ProductType) => product.isFeatured === true)
+
+
     return (
         <div className="flex flex-col">
             <h2 className="uppercase text-sm text-center my-20">Featured Mugs</h2>
             <div className="flex w-[50vw] gap-5 h-[550px] mx-auto max-xl:w-[60vw] max-lg:h-[450px] max-md:w-[80vw] max-md:h-[350px] max-sm:flex-col max-sm:h-fit">
-                {featuredProducts.map(item =>
+                {isFeatured.map((item : ProductType) =>
                     <div className="flex-1 cursor-pointer" key={item.id}>
                         <Card title={item.title} img={item.cover} price={item.price} slug={item.slug} update={false} />
                     </div>
